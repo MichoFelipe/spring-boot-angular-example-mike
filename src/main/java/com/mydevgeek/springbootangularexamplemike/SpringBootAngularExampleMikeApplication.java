e5e34067df82698da58230002727be2a1463f9c7
Michael <mfelipe.ayala@gmail.com>
2019-02-07 15:27:56 -0500

    Add all my files

---
+++ src/main/java/com/mydevgeek/springbootangularexamplemike/SpringBootAngularExampleMikeApplication.java
@@ -0,0 +1,12 @@
+package com.mydevgeek.springbootangularexamplemike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularExampleMikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularExampleMikeApplication.class, args);
+	}
+}
